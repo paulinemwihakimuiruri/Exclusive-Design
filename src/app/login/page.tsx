31a48a00ba594888961c4462f2db7726bb77a209
paulinemwihakimuiruri <paulinemwihakimuiruri@gmail.com>
2025-08-20 07:38:13 +0300

    Created signup and login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import Button from "@/shared-components/Button";
 import Image from "next/image";
 import Link from "next/link";
 
-const SignUp = () => {
+const LogIn = () => {
     return (
         <section className="max-w-7xl  mx-auto w-full pt-10 pb-13 justify-center">
             <div className="flex items-center gap-10">
@@ -12,21 +12,20 @@ const SignUp = () => {
                     <Image src="/Signup.png" alt="Side image" width={805} height={781} />
                 </div>
                 <div className="flex flex-col gap-4">
-                    <h1 className="text-4xl font-bold">Create an account</h1>
+                    <h1 className="text-4xl font-bold">Log in to Exclusive</h1>
                     <p>Enter your details below</p>
                     <div className="flex flex-col gap-4">
-                        <p className="text-gray-400 border-b">Name</p>
                         <p className="text-gray-400 border-b">Email or Phone Number</p>
                         <p className="text-gray-400 border-b">Password</p>
                     </div>
-                    <Button text="Create Account"/>
-                    <p className="flex items-center justify-between border rounded-sm p-2">Sign up with Google</p>
-                    <p className="flex items-center justify-center">Already have account? <Link href="/login">Login</Link></p>
-                   
+                    <div className="flex justify-center gap-10">
+                        <Button text="Log in"/>
+                        <p className="flex items-center justify-center text-red-600">Forgot Password</p>
+                    </div>           
                 </div>
             </div>
         </section>
     )
 }
 
-export default SignUp;
+export default LogIn;
